b5db6441cdf58af25c78a65260dc86391d31224a
lev243 <levvohmakov@gmail.com>
2026-01-15 19:32:12 +0500

    added game logic

--- main.py
+++ main.py
@@ -33,18 +33,26 @@ class Ball(GameSprite):
         super().__init__(player_img, player_x, player_y, player_speed, wight, height)
         self.player_speed_y = player_speed
     def update(self, pl_1, pl_2):
-        self.rect.x += self.player_speed
+        self.rect.x += self.speed
         self.rect.y += self.player_speed_y
         if self.rect.y <= 0 or self.rect.y >= 800 - self.rect.height:
             self.player_speed_y *= -1
         if self.rect.colliderect(pl_1.rect) or self.rect.colliderect(pl_2.rect):
-            self.player_speed *= -1
+            self.speed *= -1
         
-        
+pl_l = Platform('player-left.png', 10, 300, 5, 30, 200, K_w, K_s)
+pl_r = Platform('player-right.png', 860, 300, 5, 30, 200, K_UP, K_DOWN) 
+ball = Ball('ball.png', 400, 350, 3, 100, 100) 
 while game:
     window.fill(color_fon)
     for e in event.get():
         if e.type == QUIT:
             game = False
+    pl_l.update()
+    pl_r.update()
+    ball.update(pl_l, pl_r)
+    pl_l.reset()
+    pl_r.reset()
+    ball.reset()
     display.update()
     timer.tick(60)
